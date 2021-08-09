d0284837e35fba4434e665a0bd63b60a5b4adfe3
Yoann Deblois <yoann.deblois@gmail.com>
2021-08-09 09:26:50 +0200

    refactor: implementation of constant pattern for database file
    - DATABASE_FILE in main controller, change now easily possible

--- models/player.py
+++ models/player.py
@@ -7,7 +7,7 @@ class Player:
         self.surname = surname
         self.birth_date = birth_date
         self.sex = sex
-        self.elo_ranking = elo_ranking
+        self.elo_ranking = int(elo_ranking)
         self.tournament_score = None
         self.id = None
 
@@ -46,25 +46,34 @@ class Player:
         return player
 
     @classmethod
-    def get(cls, player_id):
+    def get(cls, player_id, database_file):
         """
-        Take id and return instance of player from the players table in db.json.
-        update the player id parameter
+        Take id and return instance of player from the players table in database file.
+        update the player id attribute
         :param player_id:
+        :param database_file:
         :return:
         """
-        player = cls.deserialize(TinyDB('db.json').table('players').get(doc_id=player_id))
+        player = cls.deserialize(TinyDB(database_file).table('players').get(doc_id=player_id))
         player.id = player_id
         return player
 
-    def store_in_database(self):
-        return TinyDB('db.json').table('players').insert(self.serialize())
+    @classmethod
+    def get_all(cls, database_file):
+        players = []
+        for player in TinyDB(database_file).table('players').all():
+            player_id = player.doc_id
+            players.append(cls.get(player_id, database_file))
+        return players
 
-    def update_in_database(self):
-        TinyDB('db.json').table('players').update(self.serialize(), doc_ids=[self.id])
+    def store_in_database(self, database_file):
+        return TinyDB(database_file).table('players').insert(self.serialize())
 
-    def save(self):
+    def update_in_database(self, database_file):
+        TinyDB(database_file).table('players').update(self.serialize(), doc_ids=[self.id])
+
+    def save(self, database_file):
         if self.id is None:
-            self.id = self.store_in_database()
+            self.id = self.store_in_database(database_file)
         else:
-            self.update_in_database()
+            self.update_in_database(database_file)
