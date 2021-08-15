3725d28b976f03d302940772138918be62340c33
Yoann Deblois <yoann.deblois@gmail.com>
2021-08-16 01:57:36 +0200

    refactor: clear_screen() now in core.utils
    refactor: ongoing tournament related views now in views.tournament

--- views/menus.py
+++ views/menus.py
@@ -1,10 +1,4 @@
-import os
-
-
-def clear_screen():
-    os.system('cls||clear') # todo test on linux
-    # os.system('cls')
-    # os.system('cls' if os.name == 'nt' else 'clear')
+from core.utils import clear_screen
 
 
 class HomeMenuView:
@@ -157,40 +151,3 @@ class TournamentInfoMenuView:
             clear_screen()
             self.display_menu()
             print("/!\\ Choix invalide /!\\")
-
-
-class InfoTournamentCreationView:
-    def __init__(self, menu_data):
-        self.menu_data = menu_data
-
-    def get_user_choice(self):
-        clear_screen()
-        return input(f"{self.menu_data.queries[0]} >> ").strip()
-
-
-class TimeControlMenuView:
-    def __init__(self, menu_data):
-        self.menu_data = menu_data
-
-    def display_menu(self):
-        for header in self.menu_data.headers:
-            print(header)
-
-        for key in self.menu_data.entries:
-            print(f"{key}: {self.menu_data.entries[key][0]}")
-        print()
-
-    def get_user_choice(self):
-        clear_screen()
-        self.display_menu()
-        while True:
-            choice = input(f"{self.menu_data.input_message} >> ").strip()
-            if choice in self.menu_data.entries:
-                return self.menu_data.entries[choice][1]
-            clear_screen()
-            self.display_menu()
-            print("/!\\ Choix invalide /!\\")
-
-
-class TournamentView:
-    pass
