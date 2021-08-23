026f64588e2c0fbbfd8ce8c8ba977099362609d6
Yoann Deblois <yoann.deblois@gmail.com>
2021-08-24 00:52:28 +0200

    docs: Finished docstrings
    refactor: change in TournamentsMenuController and TournamentsMenuView classes names for clarity
    
    style: some changes to conform to PEP8

--- views/menus.py
+++ views/menus.py
@@ -5,10 +5,22 @@ from views.utils import clear_screen
 
 
 class HomeMenuView:
+    """
+    View class for home menu, interacting with HomeMenuController.
+    A MenuData instance is used by the controller to transmit information to the view.
+    """
     def __init__(self, menu_data):
+        """
+        Init method.
+        :param menu_data: An instance of MenuData containing info to display.
+        """
         self.menu_data = menu_data
 
     def display_menu(self):
+        """
+        Displays a title and ASCII art logo.
+        Displays menu_data entries.
+        """
         print("                                    ####################")
         print("                                    # CHESS TOURNAMENT #")
         print("                                    ####################")
@@ -40,16 +52,18 @@ class HomeMenuView:
         print("                                             @")
         print()
 
+        for line in self.menu_data.lines:
+            print(line)
         for key in self.menu_data.entries:
-            if self.menu_data.entries[key][2] == "top_menu":
-                print(f"{key} : {self.menu_data.entries[key][0]}")
-        print()
-        for key in self.menu_data.entries:
-            if self.menu_data.entries[key][2] == "bottom_menu":
-                print(f"{key} : {self.menu_data.entries[key][0]}")
+            print(f"{key} : {self.menu_data.entries[key][0]}")
         print()
 
     def get_user_choice(self):
+        """
+        Clears screen then displays menu content.
+        Finally takes an user input and return choice if the input is in possible menu choices.
+        :return: A instance of a controller class depending on the choice of the user.
+        """
         clear_screen()
         self.display_menu()
         while True:
@@ -62,13 +76,23 @@ class HomeMenuView:
 
 
 class PlayersMenuView:
+    """
+    View class for home menu, interacting with PlayersMenuController.
+    A MenuData instance is used by the controller to transmit information to the view.
+    """
     def __init__(self, menu_data):
+        """
+        Init method.
+        :param menu_data: An instance of MenuData containing info to display.
+        """
         self.menu_data = menu_data
 
     def display_menu(self):
+        """
+        Displays self.menu_data's lines, top_menu entries, then bottom_menu entries.
+        """
         for line in self.menu_data.lines:
             print(line)
-
         for key in self.menu_data.entries:
             if self.menu_data.entries[key][2] == "top_menu":
                 print(f"{key.center(4)}{self.menu_data.entries[key][0]}")
@@ -79,6 +103,11 @@ class PlayersMenuView:
         print()
 
     def get_user_choice(self):
+        """
+        Clears screen then displays menu content.
+        Finally takes an user input and return choice if the input is in possible menu choices.
+        :return: A instance of a controller class depending on the choice of the user.
+        """
         clear_screen()
         self.display_menu()
         while True:
@@ -91,18 +120,34 @@ class PlayersMenuView:
 
 
 class PlayerCreationMenuView:
+    """
+    View class for home menu, interacting with PlayerCreationMenuController.
+    A MenuData instance is used by the controller to transmit information to the view.
+    """
     def __init__(self, menu_data):
+        """
+        Init method.
+        :param menu_data: An instance of MenuData containing info to display.
+        """
         self.menu_data = menu_data
 
     def display_menu(self):
+        """
+        Displays self.menu_data's lines then entries.
+        :return:
+        """
         for line in self.menu_data.lines:
             print(line)
-
         for key in self.menu_data.entries:
             print(f"{key} : {self.menu_data.entries[key][0]}")
         print()
 
     def get_user_choice(self):
+        """
+        Clears screen then displays menu content.
+        Finally takes an user input and return choice if the input is in possible menu choices.
+        :return: A instance of a controller class depending on the choice of the user.
+        """
         clear_screen()
         self.display_menu()
         if self.menu_data.entries:
@@ -118,28 +163,54 @@ class PlayerCreationMenuView:
 
 
 class ModifyPlayerEloMenuView:
+    """
+    View class for home menu, interacting with ModifyPlayerEloController.
+    A MenuData instance is used by the controller to transmit information to the view.
+    """
     def __init__(self, menu_data):
+        """
+        Init method.
+        :param menu_data: An instance of MenuData containing info to display.
+        """
         self.menu_data = menu_data
 
     def display_menu(self):
+        """
+        Displays self.menu_data's lines.
+        """
         for line in self.menu_data.lines:
             print(line)
         print()
 
     def get_user_choice(self):
+        """
+        Clears screen then displays menu content.
+        Finally takes an input and returns it.
+        :return: User input.
+        """
         clear_screen()
         self.display_menu()
         return input(f"{self.menu_data.queries[0]} >> ").strip()
 
 
-class TournamentMenuView:
+class TournamentsMenuView:
+    """
+    View class for home menu, interacting with TournamentsMenuController.
+    A MenuData instance is used by the controller to transmit information to the view.
+    """
     def __init__(self, menu_data):
+        """
+        Init method.
+        :param menu_data: An instance of MenuData containing info to display.
+        """
         self.menu_data = menu_data
 
     def display_menu(self):
+        """
+        Displays self.menu_data's lines, top_menu entries, then bottom_menu entries.
+        """
         for line in self.menu_data.lines:
             print(line)
-
         for key in self.menu_data.entries:
             if self.menu_data.entries[key][2] == "top_menu":
                 print(f"{key.center(4)}{self.menu_data.entries[key][0]}")
@@ -150,6 +221,11 @@ class TournamentMenuView:
         print()
 
     def get_user_choice(self):
+        """
+        Clears screen then displays menu content.
+        Finally takes an user input and return choice if the input is in possible menu choices.
+        :return: A instance of a controller class depending on the choice of the user.
+        """
         clear_screen()
         self.display_menu()
         while True:
@@ -162,13 +238,23 @@ class TournamentMenuView:
 
 
 class TournamentInfoMenuView:
+    """
+    View class for home menu, interacting with TournamentInfoMenuController.
+    A MenuData instance is used by the controller to transmit information to the view.
+    """
     def __init__(self, menu_data):
+        """
+        Init method.
+        :param menu_data: An instance of MenuData containing info to display.
+        """
         self.menu_data = menu_data
 
     def display_menu(self):
+        """
+        Displays self.menu_data's lines, top_menu entries, then bottom_menu entries.
+        """
         for line in self.menu_data.lines:
             print(line)
-
         for key in self.menu_data.entries:
             if self.menu_data.entries[key][2] == "top_menu":
                 print(f"{key} : {self.menu_data.entries[key][0]}")
@@ -179,6 +265,11 @@ class TournamentInfoMenuView:
         print()
 
     def get_user_choice(self):
+        """
+        Clears screen then displays menu content.
+        Finally takes an user input and return choice if the input is in possible menu choices.
+        :return: A instance of a controller class depending on the choice of the user.
+        """
         clear_screen()
         self.display_menu()
         while True:
@@ -191,27 +282,52 @@ class TournamentInfoMenuView:
 
 
 class RoundsInfoMenuView:
+    """
+    View class for home menu, interacting with RoundsInfoMenuController.
+    A MenuData instance is used by the controller to transmit information to the view.
+    """
     def __init__(self, menu_data):
+        """
+        Init method.
+        :param menu_data: An instance of MenuData containing info to display.
+        """
         self.menu_data = menu_data
 
     def display_menu(self):
+        """
+        Displays self.menu_data's lines.
+        """
         for line in self.menu_data.lines:
             print(line)
-
         print()
 
     def get_user_choice(self):
+        """
+        Clears screen then displays menu content.
+        Finally takes an input and returns it.
+        :return: User input.
+        """
         clear_screen()
         self.display_menu()
         return input(f"{self.menu_data.queries[0]} >> ").strip()
 
 
 class EndScreenView:
+    """
+    View class for home menu, interacting with EndScreenController.
+    A MenuData instance is used by the controller to transmit information to the view.
+    """
     def __init__(self, menu_data):
+        """
+        Init method.
+        :param menu_data: An instance of MenuData containing info to display.
+        """
         self.menu_data = menu_data
 
     def display_menu(self):
+        """
+        Displays self.menu_data's lines.
+        """
         clear_screen()
-
         for line in self.menu_data.lines:
             print(line)
