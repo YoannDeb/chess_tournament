9bfc2737664aee2ea99c1b71120875bd3d5ef954
Yoann Deblois <yoann.deblois@gmail.com>
2021-08-24 04:46:21 +0200

    docs: docstrings added
    style: removed useless blank line

--- chess_tournament.py
+++ chess_tournament.py
@@ -5,15 +5,28 @@ from models.storage import change_database_file
 
 
 def main():
+    """
+    Main function.
+    Checks if database argument was given.
+    If so changes database file in models.storage.
+    Finally instantiate a MainController object and launches run method.
+    """
     database_file = set_database_arg()
     if database_file:
         change_database_file(database_file)
+
     controller = MainController()
     controller.run()
 
 
 def set_database_arg():
-    parser = argparse.ArgumentParser(description="Utiliser un fichier de base de donnée différent de celui par défaut (db.json).")
+    """
+    Initiate argparser argument "--databasefile" to change database file name.
+    :return: The new database's filename, or None if no argument were given.
+    """
+    parser = argparse.ArgumentParser(
+        description="Utiliser un fichier de base de donnée différent de celui par défaut (db.json)."
+    )
     parser.add_argument("-d", "--databasefile", help="nom du fichier de base de donnée à utiliser")
     args = parser.parse_args()
     return args.databasefile
